ce27a4bae36183dfbd91be051758a046f87f5379
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-01 16:30:09 -0700

    Add relative date

--- app/src/main/java/com/codepath/apps/twitter/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/twitter/TweetAdapter.java
@@ -3,6 +3,7 @@ package com.codepath.apps.twitter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,12 +13,15 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.codepath.apps.twitter.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
 
 public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView ivProfileImage;
-        public TextView tvUserName, tvBody;
+        public TextView tvUserName, tvBody, tvDate;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -25,6 +29,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
             ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
             tvUserName = itemView.findViewById(R.id.tvUserName);
             tvBody = itemView.findViewById(R.id.tvBody);
+            tvDate = itemView.findViewById(R.id.tvDate);
         }
     }
 
@@ -54,6 +59,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
 
         viewHolder.tvUserName.setText(t.user.name);
         viewHolder.tvBody.setText(t.body);
+        viewHolder.tvDate.setText(getRelativeTimeAgo(t.createdAt));
         Glide.with(context)
              .load(t.user.profileImageUrl)
              .into(viewHolder.ivProfileImage);
@@ -62,5 +68,23 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> 
     @Override
     public int getItemCount() {
         return mTweets.size();
+    }
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public static String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
